d4390b03294ae3e0803ebe57be78c89e36296def
Ali Rahimi <1aliirahimi1@gmail.com>
2023-04-01 11:01:24 +0330

    database module hilt

--- MyApp/app/src/main/java/com/example/mymvvpapp/data/repository/StudentRepository.kt
+++ MyApp/app/src/main/java/com/example/mymvvpapp/data/repository/StudentRepository.kt
@@ -3,8 +3,9 @@ package com.example.mymvvpapp.data.repository
 import com.example.mymvvpapp.data.db.StudentDao
 import com.example.mymvvpapp.data.model.Student
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class StudentRepository(private val studentDao: StudentDao) {
+class StudentRepository @Inject constructor(private val studentDao: StudentDao) {
 
     val allStudents: Flow<List<Student>> = studentDao.getAllStudents()
 
--- MyApp/app/src/main/java/com/example/mymvvpapp/viewmodel/StudentViewModel.kt
+++ MyApp/app/src/main/java/com/example/mymvvpapp/viewmodel/StudentViewModel.kt
@@ -2,21 +2,25 @@ package com.example.mymvvpapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mymvvpapp.data.db.SchoolDatabase
 import com.example.mymvvpapp.data.model.Student
 import com.example.mymvvpapp.data.repository.StudentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class StudentViewModel(application: Application) : AndroidViewModel(application) {
-    val allStudents: Flow<List<Student>>
+@HiltViewModel
+class StudentViewModel @Inject constructor(
     private val repository: StudentRepository
+) : ViewModel() {
+
+    val allStudents: Flow<List<Student>>
 
     init {
-        val studentDao = SchoolDatabase.getDatabase(application).studentDao()
-        repository = StudentRepository(studentDao = studentDao)
         allStudents = repository.allStudents
     }
 
